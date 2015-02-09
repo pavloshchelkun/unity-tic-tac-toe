be7681046555d8aac87c020e5444eb26064fc9b4
p.shchelkun <p.shchelkun@corp.nekki.ru>
2015-02-09 22:05:00 +0200

    Add board and cell scripts

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Board : MonoBehaviour
+    {
+        public const int ROWS = 3;
+        public const int COLS = 3;
+
+        [SerializeField]
+        private GameObject cellPrefab;
+
+        [SerializeField]
+        private float cellOffset = 1.5f;
+
+        private Cell[,] cells = new Cell[ROWS, COLS];
+
+        public void Clear()
+        {
+            for (int row = 0; row < ROWS; row++)
+            {
+                for (int col = 0; col < COLS; col++)
+                {
+                    cells[row, col].Init(row, col);
+                }
+            }
+        }
+
+        public bool IsDraw()
+        {
+            for (int row = 0; row < ROWS; row++)
+            {
+                for (int col = 0; col < COLS; col++)
+                {
+                    if (cells[row, col].CurrentState == Cell.State.Empty)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public bool HasWon(Cell cell)
+        {
+            return true;
+        }
+
+        private void CreateCells()
+        {
+            Transform container = (new GameObject("cells")).transform;
+            
+            container.transform.parent = transform;
+            container.transform.localPosition = new Vector3(-cellOffset, -cellOffset, 0f);
+
+            for (int row = 0; row < ROWS; row++)
+            {
+                for (int col = 0; col < COLS; col++)
+                {
+                    GameObject go = (GameObject)Instantiate(cellPrefab, Vector3.zero, Quaternion.identity);
+                    Cell cell = go.GetComponent<Cell>();
+
+                    cell.transform.parent = container;
+                    cell.transform.localPosition = new Vector3(col * cellOffset, row * cellOffset, 0f);
+
+                    cell.Init(row, col);
+
+                    cells[row, col] = cell;
+                }
+            }
+        }
+
+        private void Awake()
+        {
+            CreateCells();
+        }
+    }
+}
---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Cell : MonoBehaviour
+    {
+        public enum State
+        {
+            Empty,
+            Cross,
+            Nought
+        }
+
+        [SerializeField]
+        private GameObject spriteCross;
+
+        [SerializeField]
+        private GameObject spriteNought;
+
+        public int Row { get; private set; }
+
+        public int Col { get; private set; }
+
+        public State CurrentState { get; private set; }
+
+        public void Init(int row, int col)
+        {
+            Row = row;
+            Col = col;
+
+            name = string.Format("cell [{0}, {1}]", row, col);
+
+            Clear();
+        }
+
+        public void Set(State state)
+        {
+            CurrentState = state;
+
+            switch (state)
+            {
+                case State.Empty:
+                    spriteCross.SetActive(false);
+                    spriteNought.SetActive(false);
+                    break;
+                case State.Cross:
+                    spriteCross.SetActive(true);
+                    spriteNought.SetActive(false);
+                    break;
+                case State.Nought:
+                    spriteCross.SetActive(false);
+                    spriteNought.SetActive(true);
+                    break;
+            }
+        }
+
+        public void Clear()
+        {
+            Set(State.Empty);
+        }
+
+        public void OnClick()
+        {
+            Set(State.Cross);
+        }
+    }
+}
